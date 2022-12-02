77918d03619e660d252abece6ac7b6c874a4c7b5
ismaelahmad6333 <106573961+ismaelahmad6333@users.noreply.github.com>
2022-12-02 06:58:39 +0000

    added Navbar/Hero Section components

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,19 @@
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <h1>
+          <span>🖨️</span>Elon Code Review
+        </h1>
+        <ul>
+          <li>Features</li>
+          <li>Testimonials</li>
+          <li>Pricing</li>
+        </ul>
+        <span>Sign in</span>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
